4ffcd6c827f78a36aae3ba1f91511baef7d334ee
Dave Judge <143704883+lvl7wizard@users.noreply.github.com>
2024-04-09 15:39:23 +0100

    add styling to solarpanel component (border, alignment)

--- components/SolarPanel.jsx
+++ components/SolarPanel.jsx
@@ -1,21 +1,29 @@
 import styled from "styled-components"
 
+const Container = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+`
+
 const SolarPanelScreen = styled.div`
-height: 50px;
+height: 40px;
 width: 100%;
 background-color: #2e1b0d;
+border: solid #666970 3px;
+border-radius: 5px;
 `
 const SolarPanelText = styled.p`
-text-align: center;
-font-size: 10px;
+font-size: 7.5px;
+font-weight: bold;
 `
 
 function SolarPanel () {
     return (
-    <div>
+    <Container>
     <SolarPanelScreen/>
     <SolarPanelText>TWO WAY POWER</SolarPanelText>
-    </div>
+    </Container>
     )
 }
 
