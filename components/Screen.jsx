ef496f398ccfd1966e404b3016a5494b58698732
Dave Judge <143704883+lvl7wizard@users.noreply.github.com>
2024-04-10 20:56:04 +0100

    adjusts styling to match 8 character limit

--- components/Screen.jsx
+++ components/Screen.jsx
@@ -12,8 +12,8 @@ border-bottom: 1.5rem solid #666970;
 border-radius: 10px 10px 25px 25px;
 border-left: solid 15px #666970;
 border-right: solid 15px #666970;
-margin-left: -2.5px;
-margin-right: -2.5px;
+margin-left: -13px;
+margin-right: -13px;
 box-shadow: 0px 0px 1px 1px rgba(0, 0, 0, 0.8);
 `
 
@@ -21,6 +21,7 @@ const CalculatorText = styled.p`
 font-family: Courier New;
 font-size: 52px;
 margin: 0px;
+margin-right: 2.5px;
 `
 
 function Screen ({currentAmount, displayTotal, currentTotal, powerOn}) {
